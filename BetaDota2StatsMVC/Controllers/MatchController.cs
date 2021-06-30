a3ac7cc4fca17ed15c032c0b66d6055f5c8123e8
Anastasios Klopas <klopas2020@gmail.com>
2021-06-30 17:55:12 +0300

    heroes json

--- BetaDota2StatsMVC/Controllers/MatchController.cs
+++ BetaDota2StatsMVC/Controllers/MatchController.cs
@@ -1,6 +1,8 @@
 ﻿using BetaDota2StatsMVC.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
@@ -36,7 +38,41 @@ namespace BetaDota2StatsMVC.Controllers
                     ModelState.AddModelError(string.Empty, "Server error occured. Please contact Admin for help!");
                 }
             }
+            var a = GetAllHeroes();
+            var ba = HeroName(matches);
+            var bac = Heroes(matches);
             return View(matches);
+        }
+        public List<Hero> GetAllHeroes()
+        {
+            string SourcePath = $"C:\\Users\\sapol\\source\\repos\\BetaDota2StatsMVC\\BetaDota2StatsMVC\\Data_JSON\\heroes.json";
+            using (StreamReader r = new StreamReader(SourcePath))
+            {
+                string json = r.ReadToEnd();
+                List<int> allHeroesID = JsonConvert.DeserializeObject<List<int>>(json);
+                //List<Hero> allHeroes1 = JsonConvert.DeserializeObject<List<Hero>>(allHeroesID);
+
+                List<Hero> allHeroes = JsonConvert.DeserializeObject<List<Hero>>(json);
+
+               // Dictionary < allHeroesID,List<Hero>>=
+                //test thn lista
+                var a = allHeroes;
+                return allHeroes;
+            }
+        }
+        public string Heroes(IEnumerable<MatchViewModel> matches)
+        {
+            var heroID=matches.Select(m => m.Hero_id).FirstOrDefault();
+            var allHeroes = GetAllHeroes();
+            var heroName = allHeroes.Where(a => a.Id == heroID).Select(b => b.Localized_Name).FirstOrDefault();
+            return heroName;
+        }
+        public ActionResult HeroName(IEnumerable<MatchViewModel> matches)
+        {
+            var heroID=matches.Select(m => m.Hero_id).FirstOrDefault();
+            var allHeroes = GetAllHeroes();
+            var heroName = allHeroes.Where(a => a.Id == heroID).Select(b => b.Localized_Name).FirstOrDefault();
+            return View(heroName);
         }
     }
 }
---
+++ BetaDota2StatsMVC/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetaDota2StatsMVC.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Localized_Name { get; set; }
+        public string Primary_Attr { get; set; }
+        public string Attack_Type { get; set; }
+        public List<String> Roles { get; set; }
+    }
+}
